038722847862fb5ca01364a4619da6d4d64e2b4d
Axel-Leger <axelleger2@gmail.com>
2024-05-31 10:43:49 -0300

    se crearon los botones de la seccion de usario y se arreglo la posicion

--- header/assets/js/main.js
+++ header/assets/js/main.js
@@ -1,23 +1,27 @@
 /*botones*/
 
 const btnProductos = document.getElementById("BtnProduc");
+const bontonUsaurio = document.getElementById("botonUsuario");
+
+/*objetos modificados*/ 
 const productos = document.getElementById("contenedorProductos");
-
-// btnProductos.addEventListener("click", function(){
-
-//     partesProductos.innerHTML= ""
-
-//     productos.appendChild(partesProductos)
-
-// })
 const lista = document.getElementById("lista")
+const usario = document.getElementById("contenedorUsuario")
+
+/*eventos*/ 
 btnProductos.addEventListener("mouseenter", ()=>{
-    if(productos.className == "pepino"){
+    if(productos.className == "productosDespegableNotVisible"){
         productos.className = "botonesProductos";
     }else{
-        productos.className = "pepino";
+        productos.className = "productosDespegableNotVisible";
     }
 
 })
 
-
+bontonUsaurio.addEventListener("click", ()=>{
+    if(usario.className == "usuarioDespegableNotVisible"){
+        usario.className = "usuarioDespegableVisible";
+    }else{
+        usario.className = "usuarioDespegableNotVisible";
+    }
+})
