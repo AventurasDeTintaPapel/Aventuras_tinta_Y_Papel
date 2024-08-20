90a79a09a7591738fed485e3ec262c6888c65213
Axel-Leger <axelleger2@gmail.com>
2024-08-20 16:25:16 -0300

    se intento hacer las tarjetas

--- client/assets/js/inicio.js
+++ client/assets/js/inicio.js
@@ -43,37 +43,16 @@ function moveToLeft(){
     }
 }
 
-// const botonActivacion = document.getElementById("TitulosAsideDes")
-// const iconosManual = document.querySelector(".iconosManual");
-// const NAV = document.querySelector(".Nav");
-// const botonNav = document.querySelector(".botonNav");
-// const lupitaPepino = document.getElementById("lupitaPepino")
-
-// botonActivacion.addEventListener("click", ()=>{
-
-//     const categoriasFiltros = document.getElementById("categoriasFiltros")
-
-//     if (categoriasFiltros.className == "categoriasFiltrosOff"){
-//         categoriasFiltros.className = "categoriasFiltrosOn"
-//     }
-//     else{
-//         categoriasFiltros.className = "categoriasFiltrosOff"
-//     }
-// })
-
-
-// iconosManual.addEventListener("click",()=>{
-//     iconosManual.classList.toggle("active");
-//     NAV.classList.toggle("active");
-//     botonNav.classList.toggle("active")
-// })
-
-
-
-// lupitaPepino.addEventListener("click", ()=>{
-
-//     const buscador = document.getElementById("barraBuscadorNAV")
-
-//     buscador.classList.toggle("ON");
-
-// } )
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
