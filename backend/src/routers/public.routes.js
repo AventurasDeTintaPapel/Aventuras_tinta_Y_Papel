7b4b663a9db6a77c8be844b94884cc13c6dff676
Axel-Leger <axelleger2@gmail.com>
2024-11-13 14:13:10 -0300

    minimo cambio

--- backend/src/routers/public.routes.js
+++ backend/src/routers/public.routes.js
@@ -7,8 +7,12 @@ export const publiRouter = Router();
 
 //ruta para cargar los productos
 publiRouter.post("/cargar", subirImagen.single("imagen"), createPublic);
+
 //update publics
 publiRouter.put("/edit", sessionVerified, updateValidation, editPublics);
+
 //get publics
 publiRouter.get("/getPublication", getAllpublics);
+
+// delte publics
 publiRouter.delete("/deletePublication", sessionVerified, deletPublic);
